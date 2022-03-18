a2c93f29fd42ac6347e29945ce23a55ab12f5083
btrumsey <bret.rumsey@intel.com>
2022-03-18 15:15:58 -0700

    Fixed truststore password.

--- start_rundeck.sh
+++ start_rundeck.sh
@@ -9,7 +9,7 @@ export PATH=$PATH:${BASE_PATH}/.java/bin
 
 if [ -f "${BASE_PATH}/ssl/mysql_truststore" ]; then
     SSL_ARG="-Djavax.net.ssl.trustStore=${BASE_PATH}/ssl/mysql_truststore \
-             -Djavax.net.ssl.trustStorePassword=abc123"
+             -Djavax.net.ssl.trustStorePassword=abc123!"
 else
     SSL_ARG=""
 fi
