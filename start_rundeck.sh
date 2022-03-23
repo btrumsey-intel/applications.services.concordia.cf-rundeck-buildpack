6bf14fb58ce231163aed8fc6aa8bf77d2d034727
btrumsey <bret.rumsey@intel.com>
2022-03-23 08:40:49 -0700

    Moving SSH setup into start_rundeck.sh

--- start_rundeck.sh
+++ start_rundeck.sh
@@ -4,6 +4,34 @@ set -euo pipefail
 BASE_PATH=/home/vcap/app
 export RDECK_BASE=${BASE_PATH}/rundeck
 
+##################################################################################
+# Setup ssh key
+##################################################################################
+echo "-----> Setting SSH key"
+
+if [ -z ${RUNDECK_SSH_KEY+x} ];
+then
+    echo "       RUNDECK_SSH_KEY is not set, skipping..."
+else
+    SSH_DIR=/home/vcap/.ssh
+    SSH_KEY=${SSH_DIR}/id_rsa
+    echo "       RUNDECK_SSH_KEY is set - creating /home/vcap/.ssh/id_rsa"
+
+    mkdir -p $SSH_DIR
+    chmod 700 $SSH_DIR
+
+    echo "$RUNDECK_SSH_KEY" > $SSH_KEY
+    chmod 600 $SSH_KEY
+
+    echo "    Wrote key to $SSH_KEY"
+    cat $SSH_KEY
+fi
+
+sleep 2
+
+##################################################################################
+# Start Rundeck
+##################################################################################
 echo "-----> Making java available"
 export PATH=$PATH:${BASE_PATH}/.java/bin
 
